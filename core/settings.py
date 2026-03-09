1afce8225e4fff71a70b9541d8c065ca81568e25
rajankarmakar <rajankarmakar@hotmail.com>
2026-03-09 15:52:30 +0600

    Allow all host

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-dev-key')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
